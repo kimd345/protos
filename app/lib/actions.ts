0c336858d89d1f8d099c561f5cbdc348aaff1812
Dan Kim <kimd345@gmail.com>
2023-11-11 10:37:33 -0500

    Add invoice form

---
+++ app/lib/actions.ts
@@ -0,0 +1,37 @@
+// By adding the 'use server', you mark all the exported functions within the file
+// as server functions. These server functions can then be imported into Client
+// and Server components, making them extremely versatile.
+'use server'
+
+import { z } from 'zod'
+import { sql } from '@vercel/postgres'
+import { revalidatePath } from 'next/cache'
+
+const InvoiceSchema = z.object({
+	id: z.string(),
+	customerId: z.string(),
+	amount: z.coerce.number(),
+	status: z.enum(['pending', 'paid']),
+	date: z.string(),
+})
+
+const CreateInvoice = InvoiceSchema.omit({ id: true, date: true })
+
+export async function createInvoice(formData: FormData) {
+	const { customerId, amount, status } = CreateInvoice.parse({
+		customerId: formData.get('customerId'),
+		amount: formData.get('amount'),
+		status: formData.get('status'),
+	})
+	const amountInCents = amount * 100
+	const date = new Date().toISOString().split('T')[0]
+	// const rawFormData = Object.fromEntries(formData.entries())
+
+	await sql`
+		INSERT INTO invoices (customer_id, amount, status, date)
+		VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+	`
+
+	// https://nextjs.org/learn/dashboard-app/mutating-data#6-revalidate-and-redirect
+	revalidatePath('/dashboard/invoices')
+}
