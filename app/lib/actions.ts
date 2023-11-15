7fcea615ef78771eb40a3055b947a97a28c46c6a
Dan Kim <kimd345@gmail.com>
2023-11-15 15:32:30 -0500

    Add Update and Delete for Invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,8 +6,9 @@
 import { z } from 'zod'
 import { sql } from '@vercel/postgres'
 import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
 
-const InvoiceSchema = z.object({
+const FormSchema = z.object({
 	id: z.string(),
 	customerId: z.string(),
 	amount: z.coerce.number(),
@@ -15,7 +16,9 @@ const InvoiceSchema = z.object({
 	date: z.string(),
 })
 
-const CreateInvoice = InvoiceSchema.omit({ id: true, date: true })
+const CreateInvoice = FormSchema.omit({ id: true, date: true })
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true })
 
 export async function createInvoice(formData: FormData) {
 	const { customerId, amount, status } = CreateInvoice.parse({
@@ -33,5 +36,32 @@ export async function createInvoice(formData: FormData) {
 	`
 
 	// https://nextjs.org/learn/dashboard-app/mutating-data#6-revalidate-and-redirect
+	revalidatePath('/dashboard/invoices')
+	redirect('/dashboard/invoices')
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+	const { customerId, amount, status } = UpdateInvoice.parse({
+		customerId: formData.get('customerId'),
+		amount: formData.get('amount'),
+		status: formData.get('status'),
+	})
+
+	const amountInCents = amount * 100
+
+	await sql`
+		UPDATE invoices
+		SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+		WHERE id = ${id}
+	`
+
+	revalidatePath('/dashboard/invoices')
+	redirect('/dashboard/invoices')
+}
+
+export async function deleteInvoice(id: string) {
+	await sql`
+		DELETE FROM invoices WHERE id = ${id}
+	`
 	revalidatePath('/dashboard/invoices')
 }
